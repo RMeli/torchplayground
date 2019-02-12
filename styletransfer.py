2275b97318fce3da6c01bf19f65917466cc8276c
Rocco Meli <r.meli@bluemail.ch>
2019-02-12 22:55:13 +0000

    fix

--- styletransfer.py
+++ styletransfer.py
@@ -10,14 +10,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load image and style image
 img_name, style_name = "octopus", "hockney"
-img = load_image("data/images/" + ".jpg").to(device)
-style_img = load_image("data/images/" + ".jpg", shape=img.shape[2:]).to(device)
+img = load_image("data/images/" + img_name + ".jpg").to(device)
+style_img = load_image("data/images/" + style_name + ".jpg", shape=img.shape[2:]).to(device)
 
 # Define style transfer object
 st = style.StyleTransfer(img, style_img, device=device)
 
 # Perform style transfer
-target, targets = st.run(2500)
+target = st.run(2)
 
 plt.figure()
 plt.imshow(tensor_to_image(target))
